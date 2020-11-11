4aa7f8655062514e79b1d32288c7c4106b5e71d4
Ryan Venturino <ryan.venturino@gmail.com>
2020-11-11 13:34:52 -0500

    Create EloquentJSMailRobot.js

---
+++ EloquentJSMailRobot.js
@@ -0,0 +1,127 @@
+/*
+The result of me following along with Chapter 7: A Robot from the book Eloquent
+Javascript. I just implemented with the random decision making as I wanted to move
+on to the next chapter, so there is no pathfinding or anything. I just did this
+as an exercise on Javascript, figured I would share because I thought it was neat.
+
+Summary:
+Our project in this chapter is to build an automaton, a little program 
+that performs a task in a virtual world. 
+Our automaton will be a mail-delivery robot picking up and dropping 
+off parcels.
+
+*/
+
+const roads = [
+  "Alice's House-Bob's House",
+  "Alice's House-Cabin",
+  "Alice's House-Post Office",
+  "Bob's House-Town Hall",
+  "Daria's House-Ernie's House",
+  "Daria's House-Town Hall",
+  "Ernie's House-Grete's House",
+  "Grete's House-Farm",
+  "Grete's House-Shop",
+  "Marketplace-Farm",
+  "Marketplace-Post Office",
+  "Marketplace-Shop",
+  "Marketplace-Town Hall",
+  "Shop-Town Hall",
+];
+
+/*
+
+The array of strings isn’t very easy to work with. What we’re interested in is the destinations that we can reach from a given place.
+
+*/
+
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+  for (let [from, to] of edges.map((r) => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
+
+const roadGraph = buildGraph(roads);
+
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels
+        .map((p) => {
+          if (p.place != this.place) return p;
+          return { place: destination, address: p.address };
+        })
+        .filter((p) => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
+
+/*
+Village state: the state of the robot's place in the village, where it needs to go.
+move method: checks whether there is a road going from the current location to the destination, and if not it will return the current state because it can't move
+*/
+let first = new VillageState("Post Office", [
+  { place: "Post Office", address: "Alice's House" },
+]);
+let next = first.move("Alice's House");
+
+console.log(next.place);
+
+console.log(next.parcels);
+
+console.log(first.place);
+
+function runRobot(state, robot, memory) {
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+/*
+A delivery robot looks at the world and decides in which direction it wants to move. As such, we could say that a robot is a function that takes a VillageState object and returns the name of a nearby place.
+*/
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.place]) };
+}
+
+VillageState.random = function (parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place == address);
+    parcels.push({ place, address });
+  }
+  return new VillageState("Post Office", parcels);
+};
+
+runRobot(VillageState.random(), randomRobot);
